b96f4f6250c8476741f2e36d8fe7181a06453e39
RohailJohn <rohailjohn1508@gmail.com>
2023-09-12 15:02:15 +0500

    twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/"
+                href="https://twitter.com/rohailjohn1508"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/"
+                  href="https://twitter.com/rohailjohn1508"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
